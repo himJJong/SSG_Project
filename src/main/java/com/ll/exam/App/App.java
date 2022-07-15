8ec27e0a5487c6fb963a5e62dc6d73c42825e73b
himjjong <jjijji5728@kpu.ac.kr>
2022-07-15 16:34:34 +0900

    feat:add Prompt

--- src/main/java/com/ll/exam/App/App.java
+++ src/main/java/com/ll/exam/App/App.java
@@ -1,7 +1,23 @@
 package com.ll.exam.App;
 
+import java.util.Scanner;
+
 public class App {
     public void run(){
-        System.out.println("SSG명언 출력");
+        System.out.println("====== SSG명언 ======");
+
+        Scanner sc = new Scanner(System.in);
+        outer:
+        while(true){
+            System.out.printf("명령:");
+            String cmd = sc.nextLine().trim();
+
+            switch(cmd){
+                case "종료":
+                    break outer;
+
+            }
+        }
+        sc.close();
     }
 }
