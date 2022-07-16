74cb75bd8f7af769a0716aa4ffafb4584594a948
himjjong <jjijji5728@kpu.ac.kr>
2022-07-16 14:20:58 +0900

    feat:add delete Module

--- src/main/java/com/ll/exam/App/App.java
+++ src/main/java/com/ll/exam/App/App.java
@@ -17,7 +17,9 @@ public class App {
             System.out.printf("명령:");
             String cmd = sc.nextLine().trim();
 
-            switch(cmd){
+            Rq rq = new Rq(cmd);
+
+            switch(rq.getPath()){
                 case "종료":
                     break outer;
 
@@ -42,7 +44,30 @@ public class App {
                         System.out.printf("%d / %s / %s\n", wiseSaying_.id,wiseSaying_.content,wiseSaying_.author);
                     }
                     break;
+                case "삭제":
+                    int paramId = rq.getIntParam("id",0);
 
+                    if(paramId == 0){
+                        System.out.println("원하시는 id가 존재하지 않습니다.");
+                        continue;
+                    }
+
+                    WiseSaying wiseSaying__ = null;
+                    for(WiseSaying wiseSaying___ : wiseSayings){
+                        if(wiseSaying___.id == paramId){
+                            wiseSaying__=wiseSaying___;
+                        }
+                    }
+
+                    if(wiseSaying__==null){
+                        System.out.printf("%d번 명언은 존재하지 않습니다.\n",paramId);
+                        continue;
+                    }
+
+                    wiseSayings.remove(wiseSaying__);
+
+                    System.out.printf("%d번 명언이 삭제 되었습니다.\n",paramId);
+                    break;
             }
         }
         sc.close();
