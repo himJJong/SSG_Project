a7c790843bbe02dc5ee8f2ea9fcfb28af384903e
himjjong <jjijji5728@kpu.ac.kr>
2022-07-16 14:03:10 +0900

    feat:add Rq class

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,6 +1,7 @@
 package com.ll.exam;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.ll.exam.App.Rq;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -45,5 +46,25 @@ public class AppTest {
         output.close();
 
         assertEquals("안녕",rs);
+    }
+
+    @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
     }
 }
