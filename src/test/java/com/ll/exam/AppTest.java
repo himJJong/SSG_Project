1b217a48b3f498de4f8f830afbc2d37cb26902da
himjjong <jjijji5728@kpu.ac.kr>
2022-07-17 19:13:02 +0900

    feat: add MaptoObject

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -83,6 +83,20 @@ public class AppTest {
         assertEquals("내 사전에 불가능은 없다.", map.get("content"));
         assertEquals("나폴레옹", map.get("author"));
     }
+    @Test
+    void 파일에_있는_JSON을_객체로_변환() {
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+
+        String rs = Util.readFromFile("test_data/1.json");
+        Map<String, Object> map = Util.jsonToMap(rs);
+        WiseSaying loadedWiseSaying = new WiseSaying(map);
+
+        assertEquals(1, map.get("id"));
+        assertEquals("내 사전에 불가능은 없다.", map.get("content"));
+        assertEquals(wiseSaying, loadedWiseSaying);
+    }
 
     @Test
     void 파일에_객체를_저장() {
